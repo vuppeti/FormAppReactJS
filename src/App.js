78527d69ce8bdb1d6cec4d76c799171bec157221
vuppeti <saiprakashreddy411@gmail.com>
2023-03-16 17:18:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TakeHome from './components/TakeHome';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <TakeHome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TakeHome.js
@@ -0,0 +1,160 @@
+import React, { useState,useEffect, useRef } from "react";
+import { LineWave    } from "react-loader-spinner";
+
+function TakeHome(){
+    //let dropdownData;
+    
+    const [occupationData,setoccupationData] = useState([]);
+    const [statesData,setstatesData] = useState([]);
+    const [isFormSubmitted,setIsFormSubmitted] = useState(false); 
+    const [formErrors,setFormErrors] = useState({});
+    const [successfullySubmitted,setSuccessfullySubmitted] = useState(false);
+    const formRef = useRef();
+    const [formValues,setFormValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        occupaion: '',
+        state: ''
+        });
+     
+    const handleChange = event=>{
+        setFormValues({...formValues, [event.target.name]: event.target.value})
+    }
+    const formValidation = (props) => {
+        setFormErrors({});
+        console.log(JSON.stringify(formErrors));
+        const errors = {}
+        if(!props.name){
+            errors.name = 'Full Name is required'
+        }
+        if(!props.email){
+            errors.email = 'Email is required'
+        }
+        if( !props.occupation || props.occupation === "Occupation... *" ){
+            errors.occupaion = 'Select your Occupation'
+        }
+        if(!props.state || props.state === "Select State...*"){
+            errors.state = 'Select your state'
+        }
+        if(!props.password){
+            errors.password = 'Password is required'
+        }
+        return errors;
+    }
+    useEffect(()=>{
+        const fetchFormData=()=>{
+            fetch('https://frontend-take-home.fetchrewards.com/form')
+            .then(response => response.json())
+            .then(json => {
+                setoccupationData(json.occupations);
+                setstatesData(json['states']);
+            })
+        }
+        fetchFormData();
+    }, []);
+
+    const postFormData=(e)=>{
+        e.preventDefault();
+        //debugger;
+        setIsFormSubmitted(true);
+        setFormValues({
+            ...formValues, 
+            name: e.target.name.value, 
+            email: e.target.email.value,
+            password: e.target.password.value,
+            occupation: e.target.occupation.value,
+            state: e.target.state.value
+        });
+        
+        console.log(formErrors);
+        console.log(formValues);
+
+        //debugger;
+        const formValues1 = {
+            
+            "name": e.target.name.value,
+            "email": e.target.email.value,
+            "password": e.target.password.value,
+            "occupation": e.target.occupation.value,
+            "state": e.target.state.value
+        };
+        const fValidObj = (formValidation(formValues1));
+        
+        setFormErrors(fValidObj);
+        //debugger;
+        if(Object.keys(fValidObj).length == 0){
+            fetch('https://frontend-take-home.fetchrewards.com/form', {
+                method: "POST",
+                body: JSON.stringify(formValues1),
+                headers: {"Content-Type": "application/json"}
+            }).then(response => {
+                response.json();
+                if(response.status === 201){
+                    formReset();
+                    setSuccessfullySubmitted(true);
+                    setIsFormSubmitted(false);
+                    setTimeout(() => {
+                        setSuccessfullySubmitted(false)
+                    }, 3000)
+                }
+                console.log(response);
+            })
+            .then(jsonResponse => console.log(jsonResponse))
+        }else{setIsFormSubmitted(false)}
+        
+    }
+    const formReset = () => {
+        formRef.current.reset();
+    }
+    
+    return (
+        
+        <>
+        <form ref={formRef} onSubmit={postFormData}>
+            {successfullySubmitted && 
+            <div className="success-message">
+                <p>Thank you for submitting the form!</p>
+            </div>}
+            <div className="formGrp">
+                <input type="text" name="name"  placeholder="Full Name *" onChange={handleChange}/>
+                <div className="formErrors">{formErrors.name}</div>
+            </div> 
+            <div className="formGrp">
+                <input type="text" name="email" placeholder="Email *" onChange={handleChange}/>
+                <div className="formErrors">{formErrors.email}</div>
+            </div>
+            <div className="formGrp">
+                <select name="occupation" defaultValue={'Occupation... *'}>
+                    <option value="Occupation... *" disabled="disabled">Occupation... *</option>
+                    {occupationData.map((occupation)=><option>{occupation}</option>)}
+                </select>
+                <div className="formErrors">{formErrors.occupaion}</div>
+            </div>    
+            <div className="formGrp">
+                <select name="state" defaultValue={'Select State...*'}>
+                    <option value="Select State...*" disabled="disabled">Select State...*</option>
+                    {statesData.map(states => <option>{states.name}</option>)}
+                </select>
+                <div className="formErrors">{formErrors.state}</div>
+            </div>    
+            <div className="formGrp">
+                <input type="password" name="password" placeholder="Password *"/>
+                <div className="formErrors">{formErrors.password}</div>
+            </div>
+            
+            <button disabled={isFormSubmitted}>
+                {isFormSubmitted && <span className="mx-4 spinner-grow spinner-grow-sm "></span>}
+                Submit
+            </button>
+
+            
+        </form>
+        </>
+    )
+    
+ 
+
+}
+
+export default TakeHome
